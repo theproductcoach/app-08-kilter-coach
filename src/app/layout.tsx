d8293fe1a8579198639f207e84ae17da32d885ca
theproductcoach <will@productcoach.io>
2025-05-08 15:58:50 +0100

    Updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "KilterCoach",
   description: "Your personal Kilterboard training assistant",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
